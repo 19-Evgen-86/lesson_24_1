b1d1cfaa61a2a84c56cb500a3cb8e3544b2fcc0e
19-Evgen-86 <ninjago58rus@gmail.com>
2022-06-02 21:58:08 +0300

    Init

--- app.py
+++ app.py
@@ -7,22 +7,8 @@ app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
-LST_PARAM = ['cmd1', 'cmd2', 'value1', 'value2', 'file_name']
+LST_PARAM = ['cmd1', 'cmd2', 'file_name']
 LST_CMD = ['filter', 'map', 'unique', 'sort', 'limit']
-
-
-def get_param(args):
-    """
-    проверяем наличие параметров в запросе. если все есть, то возвращаем словарь
-    :param args:
-    :return:
-    """
-    check_param = lambda lst_param, args: [param for param in lst_param if param not in args.keys()]
-    no_param = check_param(LST_PARAM, args)
-    if not no_param:
-        return {key: value for key, value in args.items()}
-    else:
-        return f" отсутствуют параметры {no_param}", 400
 
 
 def check_file(file_name):
@@ -40,18 +26,37 @@ class MyExc(Exception):
 def use_command(cmd, value, obj):
     match cmd:
         case 'filter':
-            return filter(lambda line: line == value, obj)
+            return list(filter(lambda line: value in line, obj))
         case "map":
-            return list(map(lambda line: line.split(), obj))[int(value)]
+            return list(map(lambda line: line.split()[int(value)], obj))
         case 'unique':
             return set(obj)
         case 'sort':
             revers = value == "desc"
-            return sorted(obj, reverse=revers)
+            return list(sorted(obj, reverse=revers))
         case 'limit':
-            return list(obj)[:value]
+            return list(obj[:int(value)])
         case _:
             raise MyExc("no command")
+
+
+def get_file_name(agrs):
+    file_name = agrs.get("file_name")
+    if file_name:
+        return check_file(file_name)
+    else:
+        raise MyExc("file not found")
+
+
+def get_param(args):
+    permitted_command = ['filter', 'map', 'unique', 'sort', 'limit']
+    commands = {key: value for key, value in request.args.items()}
+    del commands['file_name']
+    no_cmd = [cmd for cmd in permitted_command if cmd not in cmd]
+    if no_cmd:
+        raise MyExc(f'{no_cmd} не известные команды')
+    else:
+        return commands
 
 
 @app.route("/perform_query", methods=["GET"])
@@ -61,15 +66,24 @@ def perform_query():
     # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
     # вернуть пользователю сформированный результат
     try:
-        param = get_param(request.args)
-        file = check_file(param["file_name"])
+        # получаем дескриптор файла
+        file = get_file_name(request.args)
+        # получаем словарь с командами
+        commands = get_param(request.args)
+        # создаем генератор для чтения файла
         lines = (line for line in open(file))
-        print(lines)
 
-        return app.response_class('ok', content_type="text/plain")
+        for cmd, value in commands.items():
+            lines = use_command(cmd, value, lines)
+
+        result = '\n'.join(lines)
+
+        return app.response_class(result, content_type="text/plain"), 200
+
     except MyExc as e:
-        return app.response_class(e, content_type="text/plain")
+
+        return app.response_class(str(e), content_type="text/plain"), 400
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(DEBUG=True)
